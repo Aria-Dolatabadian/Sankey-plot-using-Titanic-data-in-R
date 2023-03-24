51601222830c11385a53e0e0440b7be8fc057a06
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-03-24 11:47:49 +0800

    Create Code.R

---
+++ Code.R
@@ -0,0 +1,19 @@
+#install.packages("ggalluvial")
+
+library(ggalluvial)
+
+
+ti <-read.csv("my_data.csv")
+head(ti)
+
+ggplot(data = ti,
+       aes(axis1 = Class, axis2 = Sex, axis3 = Age,
+           y = Freq)) +
+  scale_x_discrete(limits = c("Class", "Sex", "Age"), expand = c(.2, .05)) +
+  xlab("Demographic") +
+  geom_alluvium(aes(fill = Survived)) +
+  geom_stratum() +
+  geom_text(stat = "stratum", aes(label = after_stat(stratum))) +
+  theme_minimal() +
+  ggtitle("passengers on the maiden voyage of the Titanic",
+          "stratified by demographics and survival")
